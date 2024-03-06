a541e46ee812632f4d8df35f2fa9cf51b153cf42
Hotizana dealer <mtaymen@gmail.com>
2024-03-06 23:54:29 +0100

    changed client url

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-    CLIENT_URL: "",
+    CLIENT_URL: "https://fake-stake.netlify.app",
     MONGODB_URI: "mongodb+srv://smilezdemenz:e2zTjZ6sL4JgMmbr@testerog.vqdmyoi.mongodb.net/?retryWrites=true&w=majority&appName=TesterOg",
     PORT: 6969,
 }
