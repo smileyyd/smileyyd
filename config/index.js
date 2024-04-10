68ad0ea7a078b2423003fd6dd0706ceefca3d287
Hotizana dealer <mtaymen@gmail.com>
2024-04-10 21:47:45 +0100

    stakeg.com

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-    CLIENT_URL: "https://gamba.cc",
+    CLIENT_URL: "https://stakeg.com",
     //CLIENT_URL: "http://localhost:3000",
     MONGODB_URI: "mongodb+srv://smilezdemenz:e2zTjZ6sL4JgMmbr@testerog.vqdmyoi.mongodb.net/?retryWrites=true&w=majority&appName=TesterOg",
     PORT: 6969,
